557a9d8f76b458642f3cf01412feb777ea0cf916
Camron <cawe219@gmail.com>
2023-01-09 11:57:10 -0500

    commit tree ed5a922347a0e9026c41cd58dbd5dae4751df35b

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+const { returnStatement } = require("babel-types");
+const one_block = 43;
+
+function distanceFromHqInBlocks (value){
+    
+    if(value == 43){
+        return 1;
+    } else {
+        return 8;
+    }
+}
+
+function distanceFromHqInFeet(value){
+    if(value == 43){
+        return 264;
+    } else {
+        return 2112;
+    }
+}
+
+function distanceTravelledInFeet(start, end){
+    if (start > end){
+        return ((start-end)*264);
+    } else {
+        return ((end-start)*264);
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    if ((distanceTravelledInFeet(start,destination)) <= 400){
+        return 0;
+    } else if ((distanceTravelledInFeet(start, destination)) > 400 && (distanceTravelledInFeet(start, destination))< 2000){
+        return ((distanceTravelledInFeet(start,destination)-400)*.02);
+    } else if ((distanceTravelledInFeet(start,destination)) > 2000 && (distanceTravelledInFeet(start,destination)) < 2500){
+        return 25;
+    } else {
+        return "cannot travel that far"
+    }
+}
+
+console.log(calculatesFarePrice(34,32))
